87ad5aac9b70b020fe007dab26cf50ac08080127
todpig <luccasraffael6@gmail.com>
2023-11-20 18:18:43 -0300

    send to models snack e request snack and get infos in html

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,3 +1,28 @@
+import uuid
 from django.db import models
 
+def get_file_path(_instance, filename):
+    ext = filename.split('.')[-1]
+    filename = f'{uuid.uuid4()}.{ext}'
+    return filename
+class Snack(models.Model):
+    type_choices = (
+        ("almoço", "almoço"),
+        ("janta", "janta"),
+    )
+    description = models.TextField("Descrição", max_length=255)
+    likes = models.IntegerField("Curtidas", default=0)
+    image = models.ImageField("Imagem", upload_to=get_file_path, null=True)
+    snack_to_day = models.BooleanField("Refeição do dia", default=False)
+    type = models.CharField("Tipo", max_length=15, choices=type_choices)
+    active = models.BooleanField("Ativo", default=True)
 
+class RequestSnack(models.Model):
+    snack_types = Snack.type_choices
+
+    student_name = models.CharField("Nome do aluno", max_length=150)
+    student_registration = models.CharField("Matrícula do aluno", max_length=150)
+    data = models.DateField("Data")
+    justification = models.TextField("Justificativa", max_length=300)
+    status = models.BooleanField("Situação", default=False)
+    type = models.CharField("Tipo", max_length=15, choices=snack_types)
