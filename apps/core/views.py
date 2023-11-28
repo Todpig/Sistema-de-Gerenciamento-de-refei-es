856949084fab5df9c8db1375a53de2b3ebca4e43
todpig <luccasraffael6@gmail.com>
2023-11-27 21:13:33 -0300

    update form to request snack, validations and exibition message error

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
+from datetime import datetime
+from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
 from django.views.generic import TemplateView, ListView, CreateView
 from .models import Snack, RequestSnack
 from apps.core.forms import RequestSnackForm
 from django.contrib.auth import logout
+from django.contrib import messages
 class IndexView(TemplateView):
     template_name = "index.html"
 
@@ -19,16 +19,48 @@ class TodayMenu(ListView):
         context["janta"] = Snack.objects.filter(type="janta", snack_to_day=True, active=True)
         return context
     
-class RequestSnackView(CreateView):
-    template_name = "request-snack.html"
-    form_class = RequestSnackForm
-    model = RequestSnack
-    success_url = "/"
+def update_like(request, pk):
+    snack = Snack.objects.get(pk=pk)
+    snack.likes += 1
+    snack.save()
+    context = {}
+    context["almoco"] = Snack.objects.filter(type="almoço", snack_to_day=True, active=True)
+    context["janta"] = Snack.objects.filter(type="janta", snack_to_day=True, active=True)
+    return render(request, "todayMenu.html", context)
 
-    def get_context_data(self, **kwargs):
-        context = super(RequestSnackView, self).get_context_data(**kwargs)
-        context['form'] = RequestSnackForm()  
-        return context
+def request_snack_view(request):
+    if request.method == 'POST':
+        form = RequestSnackForm(request.POST)
+        form.instance.created_by = request.user
+
+        try:
+            form.instance.data = datetime.strptime(form.data['data'], '%d/%m/%Y').strftime('%Y-%m-%d')
+        except ValueError:
+            messages.error(request, 'Formato de data inválido. Use o formato DD/MM/AAAA.')
+            return render(request, 'request-snack.html', {'form': form})
+
+        existing_request = RequestSnack.objects.filter(
+            student_registration=request.user.username,
+            data=form.instance.data,
+            type=form.data['type']
+        )
+        if existing_request.exists():
+            messages.error(request, 'Vocêe já fez uma solicitação para esta data, por favor escolha outra data.')
+        elif form.is_valid():
+            form.instance.student_name = request.user.first_name + " " + request.user.last_name
+            form.instance.student_registration = request.user.username
+
+            form.save()
+            messages.success(request, 'Solicitação enviada com sucesso.')
+            return redirect('index')
+        else:
+            messages.error(request, 'Formulário inválido. Corrija os erros abaixo.')
+            print(form.errors)
+    else:
+        form = RequestSnackForm()
+
+    return render(request, 'request-snack.html', {'form': form})
+
 class AllRequestMealView(ListView):
     model = RequestSnack
     template_name = "allRequestMeal.html"
