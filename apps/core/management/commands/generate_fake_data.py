32d05e11f18bf7dfee2f8a5c9377521bc5057c56
Lobao-69 <lobomaia5@gmai.com>
2023-12-12 20:07:42 -0300

    feat: add page to my request and modify to faker data

--- apps/core/management/commands/generate_fake_data.py
+++ apps/core/management/commands/generate_fake_data.py
@@ -13,20 +13,22 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         self.stdout.write(self.style.SUCCESS('Starting data population...'))
 
-        self.create_fake_requests()
-        self.create_fake_users()
+        # Create fake users
+        users = self.create_fake_users()
+
+        # Create fake requests using existing users
+        self.create_fake_requests(users)
 
         self.stdout.write(self.style.SUCCESS('Data population completed.'))
 
-    def create_fake_requests(self, num_requests=100):
+    def create_fake_requests(self, users, num_requests=100):
         snack_types = [choice[0] for choice in Snack.type_choices]
         snack_status = [choice[0] for choice in RequestSnack.snack_status]
 
         for _ in range(num_requests):
+            user = random.choice(users)
             RequestSnack.objects.create(
-                student_name=fake.name(),
-                student_registration=fake.random_int(min=1000, max=9999),
-                student_email="luccasraffael6@gmail.com",
+                user=user,
                 data=fake.date_between(start_date='-30d', end_date='today'),
                 justification=fake.text(100),
                 status="pendente",
@@ -35,14 +37,17 @@ class Command(BaseCommand):
             )
 
     def create_fake_users(self, num_users=20):
+        users = []
         for _ in range(num_users):
-            User.objects.create_user(
+            user = User.objects.create_user(
                 username=fake.user_name(),
                 email=fake.email(),
                 password=fake.password(),
                 first_name=fake.first_name(),
                 last_name=fake.last_name(),
             )
+            users.append(user)
+        return users
 
 if __name__ == "__main__":
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'generate_fake_data.settings')
