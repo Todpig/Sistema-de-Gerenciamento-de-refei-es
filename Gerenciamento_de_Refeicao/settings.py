e829f8856848779c1b506fc9ee1684ad33882451
todpig <luccasraffael6@gmail.com>
2023-12-19 15:17:33 -0300

    modify bd for aws

--- Gerenciamento_de_Refeicao/settings.py
+++ Gerenciamento_de_Refeicao/settings.py
@@ -89,10 +89,10 @@ WSGI_APPLICATION = 'Gerenciamento_de_Refeicao.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'gerenciamento',
-        'USER': 'postgres',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
         'PORT': '5432',
     }
 }
